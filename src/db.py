3db072d1b0b112d5dc5d7520b348a328451485d5
PerchunPak <git@perchun.it>
2024-04-09 16:36:32 +0200

    Add corrupted links

--- src/db.py
+++ src/db.py
@@ -7,6 +7,7 @@ from src import utils
 class DatabaseStructure(t.TypedDict):
     all_links: set[str]
     links: set[str]
+    corrupted_links: set[str]
     delayed_messages: dict[int, set[str]]
 
 
@@ -21,6 +22,7 @@ class Database(metaclass=utils.Singleton):
         result: DatabaseStructure = {
             "all_links": set(),
             "links": set(),
+            "corrupted_links": set(),
             "delayed_messages": {},
         }
 
@@ -36,6 +38,7 @@ class Database(metaclass=utils.Singleton):
         result: DatabaseStructure = {
             "all_links": set(data["all_links"]),
             "links": set(data["links"]),
+            "corrupted_links": set(data["corrupted_links"]),
             "delayed_messages": {},
         }
 
@@ -49,6 +52,7 @@ class Database(metaclass=utils.Singleton):
         result = {
             "all_links": list(data["all_links"]),
             "links": list(data["links"]),
+            "corrupted_links": list(data["corrupted_links"]),
             "delayed_messages": {},
         }
 
@@ -61,9 +65,14 @@ class Database(metaclass=utils.Singleton):
         with self.DATABASE_PATH.open("w") as f:
             json.dump(self._unsanitize_json(self.data), f, indent=2, ensure_ascii=False)
 
-    def add_links(self, link: set[str]) -> None:
-        self.data["all_links"].update(link)
-        self.data["links"].update(link)
+    def add_links(self, links: set[str]) -> None:
+        for link in links:
+            if actual_link := utils.validate_link(link):
+                self.data["all_links"].add(actual_link)
+                self.data["links"].add(actual_link)
+            else:
+                self.data["corrupted_links"].add(link)
+
         self.save()
 
     def add_delayed_messages(self, msg: set[str], *, to: int) -> None:
