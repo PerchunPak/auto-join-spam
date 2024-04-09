35b7249b7654344d31c5071c9fa21d6321cd5139
PerchunPak <git@perchun.it>
2024-04-09 16:37:33 +0200

    Fix small typo

--- src/__main__.py
+++ src/__main__.py
@@ -36,7 +36,7 @@ async def loop(client: telethon.TelegramClient) -> None:
         try:
             await process.join_all_links(client, db.data["links"])
         except process.RateLimitError as error:
-            logger.warning("Got rate limited, sleeping for {error.sleep_for} seconds!")
+            logger.warning(f"Got rate limited, sleeping for {error.sleep_for} seconds!")
             await asyncio.sleep(error.sleep_for)
         else:
             await process.send_delayed_messages(db.data["delayed_messages"])
