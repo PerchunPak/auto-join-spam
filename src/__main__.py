fc18ed817cb4bd68da5792c36e066458614e28dc
PerchunPak <git@perchun.it>
2024-04-13 15:46:30 +0200

    Sleep for 15 minutes on rate limit

--- src/__main__.py
+++ src/__main__.py
@@ -45,12 +45,12 @@ async def loop(client: telethon.TelegramClient) -> None:
             await process.join_all_links(client, db.data["links"])
         except process.RateLimitError as error:
             logger.warning(
-                f"Got rate limited for {timedelta(seconds=error.sleep_for)}! Sleeping for next 10 minutes"
+                f"Got rate limited for {timedelta(seconds=error.sleep_for)}! Sleeping for next 15 minutes"
             )
         else:
             await process.send_delayed_messages(db.data["delayed_messages"])
 
-        await asyncio.sleep(1200)
+        await asyncio.sleep(15 * 60 * 60)
 
 
 if __name__ == "__main__":
